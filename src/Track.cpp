e8c1bf444135f33e849d0a23be6263b8bc6a1d2a
jzatta <julianokzatta@gmail.com>
2015-04-30 10:55:30 -0300

    Created a little more

--- src/Track.cpp
+++ src/Track.cpp
@@ -1,9 +1,28 @@
 
 
-public abstract class Track: public Eventable {
+public abstract class Track {
  private:
+    Queue<Vehicle> *cars;
+    int totalLenght, usedLenght, velocity;
  public:
-    bool addCar(Car *car);
-    void removeCar(Car *car);
-    bool hasSpace(int carSize);
+    Track(int lenght, int velocity){
+        this->lenght = lenght;
+        this->velocity = velocit;
+        this->usedLengt = 0;
+        cars = new Queue<Vehicle>();
+    }
+
+    Event *addCar(Vehicle *car) {
+        if ((totalLenght - usedLenght) >= car->getLenght()) {
+            cars->add(car);
+            //need to calculate time when the car is ready to go and generateEvent();
+            return generateEvent();
+        }
+        return NULL;
+    }
+
+    Vehicle *removeCar() {
+        return cars->remove();
+    }
+    Event *generateEvent(/*Event Configurations*/);
 }
